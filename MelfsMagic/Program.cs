ee5593cd71326e32dee1ce61eb1425a3f4e1ec7c
AristArcher <59206037+AristArcher@users.noreply.github.com>
2020-12-24 16:17:37 -0600

    Added Melf's Magic Store Project Directory

---
+++ MelfsMagic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MelfsMagic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Melf's Magic Store!");
+            Console.WriteLine("\n\t\tWould you like to login?");
+            Console.WriteLine("\t1 for Yes");
+            Console.WriteLine("\t2 for No");
+        }
+    }
+}
